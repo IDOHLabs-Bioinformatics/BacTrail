42f75481e80deb87f6e0247ade3f8e73691ac31d
David Schaeper <dwschaeper12@gmail.com>
2024-06-06 13:25:27 -0400

    chewBBACA schema id finder

---
+++ bin/schema_ids.py
@@ -0,0 +1,25 @@
+import requests
+import json
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--organism', required=True)
+    args = parser.parse_args()
+
+    return args.organism
+
+
+if __name__ == '__main__':
+    # initialize variables
+    organism = parse_args()
+    species_id_url = 'https://chewbbaca.online/NS/api/species/list'
+
+    # get species id
+    id_json = requests.get('https://chewbbaca.online/NS/api/species/list')
+    data = json.loads(id_json.text)
+    for entry in data:
+        if entry['name']['value'] == organism:
+            print(entry['species']['value'][-1])
+            break
