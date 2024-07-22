eea35ed583f273ff37dd498df237af254c6bcdcf
David Schaeper <dwschaeper12@gmail.com>
2024-07-22 14:25:50 -0400

    add intermediate results sqlite3 database script

---
+++ bin/update_db.py
@@ -0,0 +1,51 @@
+import argparse
+
+import sqlite3
+from contextlib import closing
+
+
+def check_table(c):
+    tables = c.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+    print(tables)
+
+    if ('intermediate',) in tables:
+        print('here')
+        return True
+    else:
+        return False
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--db_name', required=True)
+    parser.add_argument('-i', '--id', required=True)
+    parser.add_argument('-o', '--organism', required=True)
+    parser.add_argument('-a', '--assembly', required=True)
+    parser.add_argument('-g', '--gff', required=True)
+    parser.add_argument('-f', '--fasta', required=True)
+    parser.add_argument('-v', '--vcf', required=True)
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+if __name__ == '__main__':
+    # initialize variables
+    args = parse()
+
+    # open the database
+    with closing(sqlite3.connect(args.db_name)) as conn:
+        # create the cursor
+        cursor = conn.cursor()
+
+        # make the table if not already there
+        if not check_table(cursor):
+            cursor.execute(
+                "CREATE TABLE intermediate (ID TEXT, organism TEXT, assembly TEXT, gff TEXT, aligned TEXT, vcf TEXT)")
+
+        # test insert
+        cursor.execute("INSERT INTO intermediate VALUES (?, ?, ?, ?, ?, ?)", (args.id, args.organism, args.assembly,
+                                                                              args.gff, args.fasta, args.vcf))
+
+        # save the updates
+        conn.commit()
