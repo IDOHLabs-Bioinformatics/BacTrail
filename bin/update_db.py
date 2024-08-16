7403c9edccedd7817a9e9ebb3766a89e85148161
David Schaeper <dwschaeper12@gmail.com>
2024-08-16 09:00:44 -0400

    add reference genome table

--- bin/update_db.py
+++ bin/update_db.py
@@ -4,18 +4,26 @@ import sqlite3
 from contextlib import closing
 
 
+def reference_insert(c, organism, ref):
+    references = c.execute("SELECT organism FROM reference_genomes").fetchall()
+    if (organism,) in references:
+        return
+    else:
+        c.execute("INSERT INTO reference_genomes VALUES (?, ?)", (organism, ref))
+
+
 def contents(path):
     with open(path, 'r') as infile:
         data = infile.readlines()
-        data = '\n'.join(data)
+        data = ''.join(data)
 
     return data
 
 
-def check_table(c):
+def check_table(c, tname):
     tables = c.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
 
-    if ('intermediate',) in tables:
+    if (tname,) in tables:
         return True
     else:
         return False
@@ -30,6 +38,7 @@ def parse():
     parser.add_argument('-g', '--gff', required=True)
     parser.add_argument('-f', '--fasta', required=True)
     parser.add_argument('-v', '--vcf', required=True)
+    parser.add_argument('-r', '--reference', required=True)
     arguments = parser.parse_args()
 
     return arguments
@@ -44,15 +53,23 @@ if __name__ == '__main__':
         # create the cursor
         cursor = conn.cursor()
 
-        # make the table if not already there
-        if not check_table(cursor):
+        # make the intermediate table if not already there
+        if not check_table(cursor, 'intermediate'):
             cursor.execute(
-                "CREATE TABLE intermediate (ID TEXT, organism TEXT, assembly TEXT, gff TEXT, aligned TEXT, vcf TEXT)")
+                "CREATE TABLE intermediate (ID TEXT PRIMARY KEY, organism TEXT, assembly TEXT, gff TEXT, aligned TEXT, vcf TEXT)")
 
-        # test insert
+        # make the reference genome table if not already there
+        if not check_table(cursor, 'reference_genomes'):
+            cursor.execute(
+                "CREATE TABLE reference_genomes (organism TEXT PRIMARY KEY, sequence TEXT)")
+
+        # insert data
         cursor.execute("INSERT INTO intermediate VALUES (?, ?, ?, ?, ?, ?)",
                        (args.id, args.organism, contents(args.assembly),
                         contents(args.gff), contents(args.fasta), contents(args.vcf)))
+
+        # insert reference genome if not present
+        reference_insert(cursor, args.organism, contents(args.reference))
 
         # save the updates
         conn.commit()
